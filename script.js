966b993d421f34835816879104d3af2df09b2584
katiec <katiecampbell21@outlook.com>
2020-09-17 16:29:22 -0400

    created dynamic date/time

---
+++ script.js
@@ -0,0 +1,11 @@
+// display surrent time 
+// slick evenet to save text area content to local storage
+// when app opens display content for each block from local storage
+
+
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+
+var currentHour = moment().hour();
+
+
