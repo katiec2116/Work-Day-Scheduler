ae55008c5d72bb002b593d4dbae7db869904d9cd
katiec <katiecampbell21@outlook.com>
2020-09-17 19:02:45 -0400

    add classes to each element

--- script.js
+++ script.js
@@ -10,12 +10,22 @@ var currentHour = moment().hour();
 
 // Create  9 columns with 3 rows
 for (i = 0; i< 9; i++){
-    // create row fo each hour and 3 columns
 
+    // create row for each hour
     var row = $("<form>");
+    // first column
     var hour = $("<div>");
+    // last column (button)
     var button = $("<button>");
+    // middle column
     var textContent = $("<div>");
+
+    // add classes to each element created  and format row width
+    row.addClass("row");
+    hour.addClass("col-md-1 time-block hour");
+    button.addClass("saveBtn col-md-1");
+    textContent.addClass("col-md-9");
+
 
 
 }
